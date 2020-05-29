42769be232c5c45decf9e756dc5b8a5d9a4335e3
louisjiang <493509534@qq.com>
2020-05-29 09:40:51 +0800

    HotThreads

--- tava-web/src/main/java/io/tava/web/controller/ApiRestController.java
+++ tava-web/src/main/java/io/tava/web/controller/ApiRestController.java
@@ -12,23 +12,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/tava")
 public class ApiRestController {
 
-    public ApiRestController() {
-        Thread thread = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                while (true) {
-                    try {
-                        Thread.sleep(10);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-        });
-        thread.setName("teeeeeeeeeee");
-        thread.start();
-    }
-
     @RequestMapping(value = "/hotThreads", produces = "text/plain;charset=UTF-8")
     public String hotThreads(HotThreads hotThreads) throws Exception {
         return hotThreads.detect();
