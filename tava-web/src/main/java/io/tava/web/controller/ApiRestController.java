beeda4289bd5adf5fbe886ce9cfda9d8d4d97927
louisjiang <493509534@qq.com>
2021-03-02 10:07:39 +0800

    threads

--- tava-web/src/main/java/io/tava/web/controller/ApiRestController.java
+++ tava-web/src/main/java/io/tava/web/controller/ApiRestController.java
@@ -1,6 +1,7 @@
 package io.tava.web.controller;
 
 import io.tava.util.HotThreads;
+import io.tava.util.Threads;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,6 +16,11 @@ public class ApiRestController {
     @RequestMapping(value = "/hotThreads", produces = "text/plain;charset=UTF-8")
     public String hotThreads(HotThreads hotThreads) throws Exception {
         return hotThreads.detect();
+    }
+
+    @RequestMapping(value = "/threads", produces = "text/plain;charset=UTF-8")
+    public String threads() {
+        return Threads.getInstance().detect();
     }
 
 }
---
+++ tava/src/main/java/io/tava/util/Threads.java
@@ -0,0 +1,100 @@
+package io.tava.util;
+
+import java.lang.management.*;
+
+/**
+ * @author louisjiang <493509534@qq.com>
+ * @version 2021-02-26 17:21
+ */
+public class Threads implements Util {
+
+    public static final Threads threads = new Threads();
+
+    public static Threads getInstance() {
+        return threads;
+    }
+
+    private Threads() {
+    }
+
+    public String detect() {
+        ThreadMXBean threadBean = ManagementFactory.getThreadMXBean();
+        if (!threadBean.isThreadCpuTimeSupported()) {
+            throw new RuntimeException("thread CPU time is not supported on this JDK");
+        }
+
+
+        long currentThreadId = Thread.currentThread().getId();
+        long[] threadIds = threadBean.getAllThreadIds();
+        List<ThreadInfo> threadInfoList = new ArrayList<>();
+
+        for (long threadId : threadIds) {
+            if (currentThreadId == threadId) {
+                continue;
+            }
+            ThreadInfo info = threadBean.getThreadInfo(threadId, Integer.MAX_VALUE);
+            if (info == null) {
+                continue;
+            }
+            threadInfoList.add(info);
+        }
+        StringBuilder builder = new StringBuilder();
+        builder.append("peak thread:").append(threadBean.getPeakThreadCount());
+        builder.append(",daemon thread:").append(threadBean.getDaemonThreadCount());
+        builder.append(",total started thread:").append(threadBean.getTotalStartedThreadCount());
+        builder.append(",thread:").append(threadBean.getThreadCount());
+        builder.append(",blocked:").append(threadInfoList.count(threadInfo -> threadInfo.getThreadState().equals(Thread.State.BLOCKED)));
+        builder.append(",runnable:").append(threadInfoList.count(threadInfo -> threadInfo.getThreadState().equals(Thread.State.RUNNABLE)));
+        builder.append(",waiting:").append(threadInfoList.count(threadInfo -> threadInfo.getThreadState().equals(Thread.State.WAITING)));
+        builder.append(",timed_waiting:").append(threadInfoList.count(threadInfo -> threadInfo.getThreadState().equals(Thread.State.TIMED_WAITING)));
+        builder.append("\r\n\r\n");
+        for (ThreadInfo info : threadInfoList) {
+            builder.append("\"").append(info.getThreadName()).append("\"");
+            builder.append(" id=").append(info.getThreadId());
+            Thread.State threadState = info.getThreadState();
+            builder.append(" ").append(threadState);
+            String lockName = info.getLockName();
+            if (nonEmpty(lockName)) {
+                builder.append(" on ").append(lockName);
+            }
+            String lockOwnerName = info.getLockOwnerName();
+            if (nonEmpty(lockOwnerName)) {
+                builder.append(" owned by ").append(lockOwnerName).append(" id=").append(info.getLockOwnerId());
+            }
+            if (info.isSuspended()) {
+                builder.append(" (suspended)");
+            }
+            if (info.isInNative()) {
+                builder.append(" (in native)");
+            }
+            builder.append("\r\n");
+            StackTraceElement[] stackTraces = info.getStackTrace();
+            int length = stackTraces.length;
+            int index = 0;
+            while (index < length) {
+                StackTraceElement stackTrace = stackTraces[index];
+                builder.append("\t").append(stackTrace.toString()).append("\r\n");
+                if (index == 0 && info.getLockInfo() != null) {
+                    builder.append("\t- ").append(threadState).append(" on ").append(info.getLockInfo()).append("\r\n");
+                }
+                for (MonitorInfo lockedMonitor : info.getLockedMonitors()) {
+                    if (lockedMonitor.getLockedStackDepth() == index) {
+                        builder.append("\t- LOCKED MONITOR on").append(lockedMonitor).append(",locked stack frame:").append(lockedMonitor.getLockedStackFrame().toString()).append("\r\n");
+                    }
+                }
+                index++;
+            }
+            LockInfo[] lockInfos = info.getLockedSynchronizers();
+            if (nonEmpty(lockInfos)) {
+                builder.append("\r\n\tNumber of locked synchronizers = ").append(lockInfos.length);
+                for (LockInfo lockInfo : lockInfos) {
+                    builder.append("\t- ").append(lockInfo).append("\r\n");
+                }
+            }
+            builder.append("\r\n");
+        }
+
+        return builder.toString();
+    }
+
+}
